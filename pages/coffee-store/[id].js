2a195c104a20388a10249531318d71fb2a9e96d0
pankaj-kumar-mishra <pmishra403@gmail.com>
2022-01-28 19:48:09 +0530

    static data used

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,19 +1,147 @@
-import Link from "next/link";
+import Head from "next/head";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+import cls from "classnames";
 
-const CoffeeStore = () => {
-  const { query } = useRouter();
+import coffeeStoresData from "../../data/coffee-stores.json";
+import styles from "../../styles/coffee-store.module.css";
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  // console.log(context);
+  // {
+  //   params: { id: '1' },
+  //   locales: undefined,
+  //   locale: undefined,
+  //   defaultLocale: undefined
+  // }
+
+  return {
+    props: {
+      // ? propsData || {} => fixed the "Error: Failed to load static props"
+      coffeeStore:
+        coffeeStoresData.find((item) => item.id.toString() === params.id) || {},
+    },
+  };
+}
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((item) => {
+    return {
+      params: {
+        id: item.id.toString(),
+      },
+    };
+  });
+
+  // console.log(paths);
+
+  return {
+    // paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    // ? it redirects to 404 page if any path not found/returned by getStaticPaths
+    // fallback: false,
+    /* 
+    it pass that static path to getStaticProps to fetch/get data for first time 
+     ? if it found then it cached it in CDN so next other users can have that data from CDN 
+    */
+    paths,
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
+  // console.log("Coffee Store Props", props);
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  console.log(props.coffeeStore);
+  if (Object.keys(props.coffeeStore).length === 0) {
+    return (
+      <div>
+        <h2>Record not found</h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </div>
+    );
+  }
+
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+
+  const handleUpvote = () => [console.log("handleUpvote")];
+
   return (
-    <div>
-      <h1>Coffee Store {query?.id}</h1>
-      <br />
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
-      <br />
-      <Link href="/coffee-store/dynamic">
-        <a>Go to dynamic</a>
-      </Link>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+        <meta name="description" content={`${name} coffee store`}></meta>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/places.svg"
+              width="24"
+              height="24"
+              alt="places icon"
+            />
+            <p className={styles.text}>{address}</p>
+          </div>
+          {neighbourhood && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src="/static/icons/nearMe.svg"
+                width="24"
+                height="24"
+                alt="near me icon"
+              />
+              <p className={styles.text}>{neighbourhood}</p>
+            </div>
+          )}
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/star.svg"
+              width="24"
+              height="24"
+              alt="star icon"
+            />
+            <p className={styles.text}>1</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvote}>
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Head from "next/head";
 import Image from "next/image";
 import Banner from "../components/banner";
+import Card from "../components/card";
+import coffeeStoresData from "../data/coffee-stores.json";
 import styles from "../styles/Home.module.css";
 
-const Home = () => {
+export async function getStaticProps(context) {
+  // * use below method to get data. Example
+  // const data = await getCoffeeStore();
+
+  return {
+    props: {
+      coffeeStores: coffeeStoresData,
+    },
+  };
+}
+
+const Home = (props) => {
+  // console.log("Home Props ", props);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -26,6 +41,27 @@ const Home = () => {
             alt="hero image"
           />
         </div>
+
+        {props.coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Toronto stores</h2>
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={
+                      coffeeStore.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
+                    href={`/coffee-store/${coffeeStore.id}`}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
       </main>
     </div>
   );
